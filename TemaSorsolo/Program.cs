219b90430e1d5da60e049b54d483057acf2dd5a4
Budaházi Bence <budahazi.bence@petrik.hu>
2021-01-20 11:35:50 +0100

    Add project files.

---
+++ TemaSorsolo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaSorsolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> temak = File.ReadAllLines("feladat-temak.txt").ToList();
+            string readline = "";
+            Random rnd = new Random();
+            while (readline == "")
+            {
+                Console.Clear();
+                Console.WriteLine(temak[rnd.Next(temak.Count)]);
+                Console.WriteLine("\nKilépéshez írj be vlamit");
+                readline = Console.ReadLine();
+            }
+        }
+    }
+}
