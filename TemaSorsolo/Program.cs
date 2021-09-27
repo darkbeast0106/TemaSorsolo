ce62e99cfbc5cc471f2a2ae655de23cba95f8360
Budaházi Bence <budahazi.bence@petrik.hu>
2021-09-27 18:21:20 +0200

    Program kiegészítése, minden témát csak egyszer sorsol

--- TemaSorsolo/Program.cs
+++ TemaSorsolo/Program.cs
@@ -14,15 +14,35 @@ namespace TemaSorsolo
             List<String> temak = File.ReadAllLines("feladat-temak.txt").ToList();
             string readline = "";
             Random rnd = new Random();
+            Console.WriteLine("Amennyiben szeretné az ismétlődést megengedni (y/n)");
+            string ismetlodesBeker = Console.ReadLine().ToLower();
+            bool ismetlodes = false;
+            if (ismetlodesBeker.Length > 0 && ismetlodesBeker[0] == 'y')
+            {
+                ismetlodes = true;
+            }
             //Enter lenyomásra új téma generálása.
-            while (readline == "")
+            while (readline == "" && temak.Count > 0)
             {
                 //Konzol űrítése
                 Console.Clear();
                 //Új téma kiratása
-                Console.WriteLine(temak[rnd.Next(temak.Count)]);
+                int sorszam = rnd.Next(temak.Count);
+                Console.WriteLine(temak[sorszam]);
                 //Instrukció kiratása
                 Console.WriteLine("\nAz újboli sorsoláshoz nyomj entert.\nKilépéshez írj be valamit az enter elé.");
+                if (!ismetlodes)
+                {
+                    temak.RemoveAt(sorszam);
+                    if (temak.Count > 0)
+                    {
+                        Console.WriteLine("Még {0} db téma van hátra", temak.Count);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nincs több téma, a program leáll.");
+                    }
+                }
                 //Felhasználó input beolvasása - ha üres akkor új témát generál, amúgy pedig kilép.
                 readline = Console.ReadLine();
             }
